87cbcb532e0d1d85eda1cc949c64841eb7e56121
Mark Wilkinson <markwilko22@gmail.com>
2021-12-14 09:09:23 +0000

    Added sample checker

--- src/helpers/fileHelper.ts
+++ src/helpers/fileHelper.ts
@@ -2,5 +2,7 @@ const path = require("path");
 import { videoFileTypes } from "../constants/videoFileTypes";
 
 export function fileIsVideo(file: string): boolean {
-	return videoFileTypes.includes(path.extname(file).toLowerCase());
+	const isVideo = videoFileTypes.includes(path.extname(file).toLowerCase());
+	const isSample = file.toLowerCase().includes("sample");
+	return isVideo && !isSample;
 }
